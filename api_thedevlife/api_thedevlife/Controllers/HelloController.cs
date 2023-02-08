29fb22ff4e7708fdb77732d1e17678a299cb9eb8
rhuan05 <rhuanhenrique964@gmail.com>
2023-02-08 17:44:27 -0300

    Subindo API

---
+++ api_thedevlife/api_thedevlife/Controllers/HelloController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace api_thedevlife.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World!");
+        }
+
+        [HttpPost]
+        public IActionResult Post(JObject payload)
+        {
+            return Ok(payload);
+        }
+
+    }
+}
